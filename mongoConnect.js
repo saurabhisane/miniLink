5bd4a8bed2617855dd6adbd7f9fda15f3d5195c1
Saurabh Isane <114484966+saurabhisane@users.noreply.github.com>
2025-07-24 13:04:59 +0530

    Update mongoConnect.js

--- mongoConnect.js
+++ mongoConnect.js
@@ -8,7 +8,10 @@ const mongoURI = process.env.MONGO_URI; // Replace with your MongoDB URI
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
